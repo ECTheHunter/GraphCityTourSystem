5c424b614edd688dc564aaf6fb16b70112f599e6
ECTheHunter <emirhancatalbas@windowslive.com>
2023-05-14 16:22:44 +0300

    .

--- src/project/Graph.java
+++ src/project/Graph.java
@@ -18,10 +18,19 @@ public class Graph<T> {
 		map.put(newvertex, new LinkedList<Edge<T>>());
 		
 	}
-	public void AddEdge(Edge<T> newedge) {
+	public void AddEdge(Edge<T> newedge, boolean bothway) {
 		LinkedList<Edge<T>> temp =  map.get(newedge.getSrc());
 		temp.add(newedge);
 		map.put(newedge.getSrc(), temp);
+		if(bothway) {
+			LinkedList<Edge<T>> tempdst =  map.get(newedge.getDst());
+			Edge<T> e = (Edge<T>)new RegionEdge();
+			e = newedge;
+			RegionEdge re = (RegionEdge)e;
+			tempdst.add((Edge<T>)new RegionEdge(re.getDst(), re.getSrc(), re.getDistance(), re.getTraveltime()));
+			map.put(newedge.getDst(), tempdst);
+			
+		}
 	}
 	public void PrintAdjacentVertices(Region vertex) {
 		LinkedList<Edge<T>> ll = map.get(vertex);
@@ -29,7 +38,7 @@ public class Graph<T> {
 			Edge<T> e = (Edge<T>)new RegionEdge();
 			e = edge;
 			RegionEdge re = (RegionEdge)e;
-			System.out.println(re.getDistance());
+			System.out.println(re.getSrc() +" "+ re.getDistance() + " "+re.getDst());
 		}
 	}
 }
--- src/project/Test.java
+++ src/project/Test.java
@@ -8,13 +8,16 @@ public class Test {
 		new mainpanel();
 		Region sisliRegion = new Region("sisli", "sislilandmarks");
 		Region besiktasRegion = new Region("besiktas", "besiktaslandmarks");
+		Region beyogluregion = new Region("beyoglu", "beyoglulandmarks");
 		Graph<Region> citymap = new Graph<Region>();
 		citymap.addVertex(besiktasRegion);
 		citymap.addVertex(sisliRegion);
-		citymap.AddEdge(new RegionEdge(besiktasRegion, sisliRegion, 5, 2));
-		citymap.AddEdge(new RegionEdge(sisliRegion, besiktasRegion, 5, 2));
+		citymap.addVertex(beyogluregion);
+		citymap.AddEdge(new RegionEdge(besiktasRegion, sisliRegion, 5, 2), true);
+		citymap.AddEdge(new RegionEdge(besiktasRegion, beyogluregion, 3, 1), true);
 		citymap.PrintAdjacentVertices(besiktasRegion);
 		citymap.PrintAdjacentVertices(sisliRegion);
+		citymap.PrintAdjacentVertices(beyogluregion);
 
 
 	}
